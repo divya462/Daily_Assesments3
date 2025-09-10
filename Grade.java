5bd4d724ba186308423d4be80e062ae0e2a904d5
Divya kotte <153913545+divya462@users.noreply.github.com>
2025-09-10 15:43:12 +0530

    Update and rename Grade Program to Grade.java

--- Grade.java
+++ Grade.java
@@ -1,3 +1,12 @@
+/*
+1) Write a Java program that prompts the user to enter their score (an integer between 0 and 100). The program should then print the corresponding grade based on the following criteria:
+A: 90-100
+B: 80-89
+C: 70-79
+D: 60-69
+F: 0-59
+Expected Output: If the user inputs "85", the program should output: "Your grade is B."
+*/
 import java.util.*;
 class Grade {
     public static void main(String[] args) {
