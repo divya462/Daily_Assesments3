caaf0b78957189a13daeabac7de29817ff54ecea
Divya kotte <153913545+divya462@users.noreply.github.com>
2025-09-10 15:49:12 +0530

    Update and rename Pattern Program to Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,4 +1,17 @@
- class Pattern {
+/* 
+8. Write a Java Program to get the following O/p.
+
+*
+* * *
+* * * * *
+* * * * * * *
+* * * * * * * * *
+* * * * * * *
+* * * * *
+* * *
+*
+ */
+class Pattern {
     public static void main(String[] args) {
         for(int i=1;i<=9;i+=2){for(int j=1;j<=i;j++) System.out.print("* ");System.out.println();}
         for(int i=7;i>=1;i-=2){for(int j=1;j<=i;j++) System.out.print("* ");System.out.println();}
